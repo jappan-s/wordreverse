c558f044e768873b43109b722d2d996def1c7faa
jappan-s <63873951+jappan-s@users.noreply.github.com>
2022-10-16 23:07:44 +0530

    Add files via upload

---
+++ wordreverse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+word = input(" input a word ")
+for char in range(len(word) -1, -1 ,-1):
+    print(word[char], end="")
+print("\n")
+
+
+# In[ ]:
+
+
+
+
